39827f069f98d13197eb320064dc35a595a1d696
shohruzjon-tech <80161192+shohruzjon-tech@users.noreply.github.com>
2021-07-05 14:23:29 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,14 +2,6 @@ import firebase from 'firebase';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC5tVdlL0K0u7wQP3Tbe1bg4WcfkIsjoFk",
-  authDomain: "uzstore.firebaseapp.com",
-  databaseURL: "https://uzstore-default-rtdb.firebaseio.com",
-  projectId: "uzstore",
-  storageBucket: "uzstore.appspot.com",
-  messagingSenderId: "519153610191",
-  appId: "1:519153610191:web:b8195300ed3f596741f6ba",
-  measurementId: "G-MNDT2KXZ7B"
 };
 
 
